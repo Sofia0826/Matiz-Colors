8e62c4b64d6480089602f09c49bb52f304309f2a
Maria Sofia Pimentel Plaza <mpimentelplaza@gmail.com>
2025-03-21 07:18:49 -0500

    ultimo push

--- staticfiles/js/messages.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const messages = document.querySelectorAll(".messages div, .mensajes p");
-    messages.forEach((msg) => {
-        setTimeout(() => {
-            msg.style.display = "none";
-        }, 3000); // Oculta los mensajes después de 3 segundos
-    });
-});
